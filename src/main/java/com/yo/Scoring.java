0b93861028d62642da00c2ccc1e9ddf52d300e32
Benny Yo <benny91917@gmail.com>
2022-03-04 16:22:42 +0800

    Scoring

---
+++ src/main/java/com/yo/Scoring.java
@@ -0,0 +1,11 @@
+package com.yo;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack=new Student();
+        jack.name="jack";
+        jack.maths=88;
+        jack.english=70;
+
+    }
+}
---
+++ src/main/java/com/yo/Student.java
@@ -0,0 +1,7 @@
+package com.yo;
+
+public class Student {
+    String name;
+    int english;
+    int maths;
+}
