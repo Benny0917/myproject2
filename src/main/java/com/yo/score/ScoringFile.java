4478453890fdbc2f2c61d87b166554a74780fc80
Benny0917 <benny91917@gmail.com>
2022-06-17 16:28:42 +0800

    BufferReader

--- src/main/java/com/yo/score/ScoringFile.java
+++ src/main/java/com/yo/score/ScoringFile.java
@@ -1,6 +1,7 @@
 package com.yo.score;
 
 import javax.imageio.IIOException;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,9 +10,19 @@ public class ScoringFile {
     public static void main(String[] args) {
         try{
             FileReader fileReader = new FileReader("score.txt");//throws
-            int data = fileReader.read();
-            data =fileReader.read();
-            System.out.println(data);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            System.out.println(line);
+            while(line != null){
+                //System.out.println(line);
+                String[] token = line.split(",");
+                String name =token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student();
+                stu.print();
+                line = reader.readLine();
+            }
         }catch(FileNotFoundException e){
             System.out.println("檔案讀取失敗");
         }catch (IOException e){
